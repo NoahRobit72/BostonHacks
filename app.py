3ff7a866a4ef80a3d3bc84f5f720226ab1320b61
Jared Solis <114105274+j-solis11@users.noreply.github.com>
2023-11-19 09:46:39 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
+from storage import *
+from story_jared import *
 from flask_cors import CORS
+from tempMain import *
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)   #Enable CORS for all routes
+# Define a route for the root URL
 
 # Define a route for handling the form submission
 @app.route('/getImage', methods=['POST'])
 def getImage():
-    # Assuming the image name is sent as a query parameter
+    # Assuming the image name is sent as form data
     image_name = request.args.get('imageName')
-
-    print("OK Received. I am now going to try to query Firebase for an image with a name: " + str(image_name))
-    # Additional logic for querying Firebase or any other operations can be added here
-
-    return "Function returns"
-
-
-@app.route('/getStatus', methods=['GET'])
-def getStatus():
-    status = 1# Replace this with your actual logic
+    image_name = "Canvas2"
+    print("OK Received. I am now going to try to query Firebase for an image with a name: " + str(image_name))    
     
-    return str(status)
+    storagemain(image_name)
+    processImage()
+    
+
+
+    return "function returns"
+
 
 
 if __name__ == '__main__':
     # Run the application on http://127.0.0.1:5000/
-    app.run(debug=True)
+   # app.run(host = '10.239.186.227', port = 5000, debug = True)
+   app.run(host = '127.0.0.1', port = 5000, debug = True)
