cdf435522e578fd7114e74cc6ca9ae3ef82934d7
Jilin <133818802+jilinzheng@users.noreply.github.com>
2023-11-19 10:29:06 -0500

    Delete mergeAudio.py

--- mergeAudio.py
+++
@@ -1,9 +0,0 @@
-import pydub
-from pydub import AudioSegment
-
-def mergeAudio(audioFiles):
-    dialogue = AudioSegment.from_file("audioFile0.wav")
-    combinedDialogue = dialogue
-    for ii in range(1,len(audioFiles)):
-        dialogue = AudioSegment.from_file(f"audioFile{ii}.wav")
-        combinedDialogue += dialogue
